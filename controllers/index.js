2f65b47037cdbf4da824ef5b51f69a2a4624a9d0
Jonathan Bridge <jvbridge@gmail.com>
2022-05-06 22:15:35 -0700

    main routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
+/**
+ * This will serve up user facing infomation
+ */
+const homeRoutes = require("./homeRoutes");
+router.use("/", homeRoutes);
+
+/**
+ * API is all non user facing queries. They will be used primarily to modify the
+ * database
+ */
 const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
