12ac0d7d0e02be65490d4c5ebd1646708cb51d4b
Jonathan Bridge <jvbridge@gmail.com>
2022-05-08 19:52:36 -0700

    seeding

--- seed/seeds.js
+++ seed/seeds.js
@@ -1,1 +1,28 @@
-// TODO: make seeds after models
+const sequelize = require("../config/connection");
+const { User, Posts } = require("../models");
+
+const userData = require("./user-data.json");
+const postData = require("./post-data.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const user of users) {
+    const postCount = Math.floor(Math.random() * postData.length);
+    for (let i = 0; i < postCount; i++) {
+      let postIndex = Math.floor(Math.random() * postData.length);
+      await user.createPost(
+        postData[postIndex].title,
+        postData[postIndex].body
+      );
+    }
+  }
+  process.exit(0);
+};
+
+seedDatabase();
