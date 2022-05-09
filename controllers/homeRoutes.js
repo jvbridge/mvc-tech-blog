6092c9fd83a752725e3629376011be47d14d5636
Jonathan Bridge <jvbridge@gmail.com>
2022-05-08 18:19:21 -0700

    login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,37 @@
 const router = require("express").Router();
 const { User } = require("../models");
+const withAuth = require("../util/auth");
 
 router.get("/", async (req, res) => {
+  try {
+    res.render("homepage", { loggedIn: req.session.loggedin });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    if (req.session.loggedin) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/logout", withAuth, async (req, res) => {
+  try {
+    req.session.destroy();
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/users", withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ["password"] },
@@ -9,11 +39,9 @@ router.get("/", async (req, res) => {
     });
 
     const users = userData.map((user) => user.get({ plain: true }));
-    console.log("Rendering: ", users);
-    res.render("homepage", { users });
+    res.render("users", { loggedin: req.session.loggedin, users });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 module.exports = router;
