35d4aa016416470ac8fb7abc2febff70f9132c0f
Jonathan Bridge <jvbridge@gmail.com>
2022-05-08 19:56:38 -0700

    basic posting

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
-const User = require("./User");
+const User = require("./user");
+const Posts = require("./posts");
 
-module.exports = { User };
+User.hasMany(Posts, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+Posts.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Posts };
---
+++ models/posts.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
+);
+
+module.exports = Posts;
