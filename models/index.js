3144cf3ba4f989a5285b2abe3e943a70236d8983
Jonathan Bridge <jvbridge@gmail.com>
2022-05-09 13:32:23 -0700

    comment models and seeds

--- models/index.js
+++ models/index.js
@@ -1,13 +1,28 @@
 const User = require("./user");
 const Posts = require("./posts");
+const Comments = require("./comments");
 
 User.hasMany(Posts, {
   foreignKey: "user_id",
-  onDelete: "cascade",
+  onDelete: "CASCADE",
 });
 
 Posts.belongsTo(User, {
   foreignKey: "user_id",
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comments, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Posts };
--- seed/seeds.js
+++ seed/seeds.js
@@ -4,14 +4,18 @@ const { User, Posts } = require("../models");
 const userData = require("./user-data.json");
 const postData = require("./post-data.json");
 
+MAX_COMMENTS = 7;
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  // seeding users
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
+  // seeding posts
   for (const user of users) {
     const postCount = Math.floor(Math.random() * postData.length);
     for (let i = 0; i < postCount; i++) {
@@ -22,6 +26,23 @@ const seedDatabase = async () => {
       );
     }
   }
+
+  // seeding comments
+  const posts = await Posts.findAll();
+
+  // every poster makes comments
+  for (const post of posts) {
+    // random number of comments
+    const commentCount = Math.floor(Math.random() * MAX_COMMENTS);
+    //
+    for (let i = 0; i < commentCount; i++) {
+      const commenterIndex = Math.floor(Math.random() * users.length);
+      const currUser = users[commenterIndex];
+      const commentIndex = Math.floor(Math.random() * postData.length);
+      await currUser.createCommment(post.id, postData[commentIndex].body);
+    }
+  }
+
   process.exit(0);
 };
 
