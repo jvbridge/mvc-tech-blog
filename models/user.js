c2221402944891456966021f26eb946a10f2cf2e
Jonathan Bridge <jvbridge@gmail.com>
2022-06-13 21:23:47 -0700

    fix the requires

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
-const sequelize = require("./config/connection");
-const Posts = require("./models/posts");
-const Comments = require("./models/comments");
+const sequelize = require("../config/connection");
+const Posts = require("./posts");
+const Comments = require("./comments");
 /**
  * User is a table used to store each individual user in the database.
  */
